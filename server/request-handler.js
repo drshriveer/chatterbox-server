c0361715ea77d3b7f07c02f7150e94ef2c691343
Gavin Shriver <gavin.shriver@gmail.com>
2013-10-14 18:10:52 -0700

    begins refactor for sending custom response codes and passes more jasmine tests

--- server/request-handler.js
+++ server/request-handler.js
@@ -6,15 +6,34 @@ var url = require('url');
  * node module documentation at http://nodejs.org/api/modules.html. */
 
 var _messages = [{username: "bill",text:"this is dummby data", roomname:"lobby", createdAt: new Date()}];
+var _chatrooms = {};//{lobby: 'lobby'};
+
 
 var handleRequest = function(request, response) {
+  var statusCode = 200;
+  var headers =  {
+    "access-control-allow-origin": "*",
+    "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "access-control-allow-headers": "content-type, accept",
+    "access-control-max-age": 10 // Seconds.
+  };
+
+  headers['Content-Type'] = "text/plain";
+  // response.writeHead(statusCode, headers);
+
   console.log("Serving request type " + request.method + " for url " + request.url);
 
   if (request.method === 'GET'){
     var pathname = url.parse(request.url).pathname;
-    if (pathname === '/1/classes/messages') {
-       response.write(JSON.stringify({results: _messages}));
-       response.end();
+    if (pathname === '/classes/messages') {
+      response.writeHead(statusCode, headers);
+       response.end(JSON.stringify({results: _messages}));
+       // response.end();
+    }
+    if (pathname === '/classes/room1') {
+       response.writeHead(statusCode, headers);
+       response.end(JSON.stringify(Object.keys(_chatrooms)));
+       // response.end();
     }
   } else if (request.method === 'POST'){
     var body = "";
@@ -26,10 +45,15 @@ var handleRequest = function(request, response) {
       POST.createdAt = new Date();
       _messages.push(POST);
     });
+    response.writeHead(201, headers);
+    response.end();
   } else if (request.method === 'OPTIONS') {
     console.log ('we have none');
   }
+
+  // response.end();
 };
+
 
 
 exports.handleRequest = handleRequest;
