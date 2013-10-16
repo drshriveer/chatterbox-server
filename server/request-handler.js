db8000202b52f57c20b72d04ef365724b211b1e7
Gavin Shriver <gavin.shriver@gmail.com>
2013-10-15 20:18:08 -0700

    works

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,12 +1,7 @@
 var url = require('url');
-/* You should implement your request handler function in this file.
- * But you need to pass the function to http.createServer() in
- * basic-server.js.  So you must figure out how to export the function
- * from this file and include it in basic-server.js. Check out the
- * node module documentation at http://nodejs.org/api/modules.html. */
+var fs = require('fs');
 
 var _chatrooms = {lobby: [{username: "bill", message: "this is dummby data", roomname: "lobby", createdAt: new Date()}]};//{lobby: 'lobby'};
-
 
 var handleRequest = function(request, response) {
   console.log("Serving request type " + request.method + " for url " + request.url);
@@ -15,11 +10,18 @@ var handleRequest = function(request, response) {
     "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
     "access-control-allow-headers": "content-type, accept",
     "access-control-max-age": 10, // Seconds.
-    "content-type": "text/plain"
+    "content-type": "application/json"
   };
 
-  var chatroom = (url.parse(request.url).pathname).split('/')[2];  //this is a hack!! fix me!
+//   fs.readFile('messages.txt', 'utf8', function( err, data ) {
+//       console.log(data);
+//       data = data.split("|");
+//       console.log(data);
+// //      data = JSON.parse(data);
 
+//   });
+
+  var chatroom = (url.parse(request.url).pathname).split('/')[2];  //this is a hack!! fix me!
   // Switches between request methods
   if (chatroom) {
     _chatrooms[chatroom] = ( _chatrooms[chatroom] ) ? _chatrooms[chatroom]: [];
@@ -27,13 +29,15 @@ var handleRequest = function(request, response) {
       handleGet(request, response, headers, chatroom);
     } else if (request.method === 'POST'){
       handlePost(request, response, headers, chatroom);
+    } else if (request.method === 'OPTIONS'){
+      response.writeHead(200, headers);
+      response.end();
     }
   } else {
     response.writeHead(404, headers);
     response.end();
   }
 };
-
 
 //
 //  ---------- GET Request Method Handlers ---------------------
@@ -55,6 +59,12 @@ var handlePost = function(request, response, headers, chatroom) {
     var POST = JSON.parse(body);
     POST.createdAt = new Date();
     _chatrooms[chatroom].push(POST);
+    // var messsageText = JSON.stringify(POST);
+    // fs.appendFile('messages.txt', messsageText, function(err) {
+    //   if (!err) {
+    //     console.log('messages updated');
+    //   }
+    // });
   });
   response.writeHead(201, headers);
   response.end();
